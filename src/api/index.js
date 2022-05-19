5494fbae65c4506abafd68264a27573720da213f
Valentin Giarra <valentingiarra2@gmail.com>
2022-05-18 21:26:12 -0300

    changed url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000'
+  baseURL: 'https://nogyj0d-tickets.onrender.com'
 })
 
 export const get = url => {
